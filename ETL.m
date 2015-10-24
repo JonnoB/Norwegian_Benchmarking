efcbdc06177df1cfebb47f2a6deb9255318ae3ce
Jonathan Bourne <g>
2015-10-24 23:35:31 +0100

    simple but does the job import

--- ETL.m
+++ ETL.m
@@ -2,19 +2,33 @@
 %using the TreatAsEmpty arguement
 
 cd ('C:\Users\Jonno\Documents\Benchmarking\PA consulting\2015\Statkraft');
-filename = 'Statkraft.xlsx'
-z = xlsread(filename, 'statkraft_powerplant')
 
 %% generator spec
 filename = 'statkraft_genspec.csv';
-formatSpec = '%s %f %s %s %s %f %f %f %f %f %C %C %C %C %C %f %f';
-Statkraft_gen = readtable(filename, 'Format', formatSpec,  'TreatAsEmpty', {'NA'});
+gen = readtable(filename,  'TreatAsEmpty', {'NA'});
+gen.Turbine_type = categorical(gen.Turbine_type) ;
+gen.Reversible_pump_turbine_ = categorical(gen.Reversible_pump_turbine_);
+gen.Kaplan___bulb_turbine_ = categorical(gen.Kaplan___bulb_turbine_);
+gen.Turbine_with_2_or_more_runners_ = categorical(gen.Turbine_with_2_or_more_runners_);
+gen.Water_cooled_windings_ = categorical(gen.Water_cooled_windings_);
+
 %% Powerplant This has something wrong in the type catagorization
 filename = 'statkraft_powerplant.csv';
-formatSpec = '%f %C %C %s %f %f %s %f %f %C %C %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f';
-Statkraft_powerplant = readtable(filename, 'Format', formatSpec,  'TreatAsEmpty', {'NA', '', ' '});
+powerplant = readtable(filename, 'TreatAsEmpty', {'NA', '', ' '});
+powerplant.Storage___Run_of_river_plant = categorical(powerplant.Storage___Run_of_river_plant);
+powerplant.Station_above_ground_ = categorical(powerplant.Station_above_ground_);
+
 %% damspec
 filename = 'statkraft_damspec.csv';
-formatSpec = '%s %s %s %s %C %C %s %s %s %s %f';
-Statkraft_damspec = readtable(filename,'Format', formatSpec,  'TreatAsEmpty', {'NA', '', ' '});
+damspec = readtable(filename,  'TreatAsEmpty', {'NA', '', ' '});
+damspec.Object_ID = categorical(damspec.Object_ID)
+damspec.Dam_or_River_stream_intake__EBI__ = categorical(damspec.Dam_or_River_stream_intake__EBI__)
 
+%% gatesspec  column problem
+filename = 'statkraft_gatespec.csv';
+gatespec = readtable(filename, 'TreatAsEmpty', {'NA', '', ' '});
+%gatespec.Object_ID = categorical(gatespec.Object_ID)
+%%
+filename = 'statkraft_valvespec.csv';
+valvespec = readtable(filename,  'TreatAsEmpty', {'NA', '', ' '});
+%%
